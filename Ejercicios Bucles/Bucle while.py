3c8c760c25377002f95c4d6dc72dc2b26511d716
ElFilgue <paufilvin@alu.edu.gva.es>
2024-10-30 13:38:34 +0100

    Ejercicios bucles 2

--- Ejercicios Bucles/Bucle while.py
+++ Ejercicios Bucles/Bucle while.py
@@ -4,4 +4,16 @@ import random
 valor_minimo = int(input("Introduce el valor mínimo: "))
 valor_maximo = int(input("Introduce el valor máximo: "))
 secreto = random.randint(valor_minimo,valor_maximo)
-print("El número secreto está entre",valor_minimo,"y",valor_maximo)
+print(f"A ver si adivinas un número entero entre {valor_minimo} y {valor_maximo}")
+intentos = 1
+numero= int(input("Escribe un número: "))
+while numero != secreto:
+    if numero < secreto:
+        numero = int(input("¡Demasiado pequeño! Intentalo de nuevo: "))
+    else:
+        numero = int(input("¡Demasiado alto! Intentalo de nuevo: "))
+    intentos += 1
+print(f"¡Acertaste! Te ha costado {intentos} intentos")
+
+
+
