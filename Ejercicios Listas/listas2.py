502675c24d30f9ac3b0a4b89fd3300ec86be12fb
ElFilgue <paufilvin@alu.edu.gva.es>
2024-11-05 13:57:16 +0100

    ejercicios lista beta

--- Ejercicios Listas/listas2.py
+++ Ejercicios Listas/listas2.py
@@ -2,9 +2,10 @@
 
 n=int(input(f"Dígame cuántas palabras tiene la lista: "))
 lista=[]
+
 for i in range(0,n):
   palabra=input(f"Dígame la palabra {1+i}: ")
   lista.append(palabra)
 print(f"La lista creada es: {lista}")
-lista_invertida=lista.reverse()
+lista_invertida=reversed(lista)
 print(f"La lista inversa es: {lista_invertida}")
