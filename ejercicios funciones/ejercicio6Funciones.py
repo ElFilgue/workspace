d1da8dc2628c68e3f91edc0f2a777a9bdee94371
ElFilgue <paufilvin@alu.edu.gva.es>
2024-11-26 13:26:00 +0100

    ejercicio corregido

--- ejercicios funciones/ejercicio6Funciones.py
+++ ejercicios funciones/ejercicio6Funciones.py
@@ -2,16 +2,14 @@ def Login(usuario, contraseña, intentos):
   if usuario == "usuario1" and contraseña == "asdasd":
     return True
   else:
-    intentos -= 1  # Decrementar intentos
+    intentos -= 1
     return False
 def Registrarse():
-  """Registra un nuevo usuario."""
   usuario = input("Ingrese un nuevo nombre de usuario: ")
   contraseña = input("Ingrese una nueva contraseña: ")
-
-intentos = 3 
-
-while True:
+  print("Usuario registrado exitosamente.")
+intentos = 3
+while intentos > 0:
   opcion = input("¿Desea iniciar sesión o registrarse? (iniciar/registrar): ")
   if opcion.lower() == "iniciar":
     while intentos > 0:
@@ -20,6 +18,7 @@ while True:
   
       if Login(usuario, contraseña, intentos):
         print("Inicio de sesión exitoso.")
+        intentos = 3
         break
       else:
         print("Nombre de usuario o contraseña incorrectos. Intentos restantes:", intentos)
@@ -28,5 +27,9 @@ while True:
       break
   elif opcion.lower() == "registrar":
     Registrarse()
+    intentos = 3
   else:
     print("Opción inválida.")
+    intentos -= 1
+if intentos == 0:
+  print("Demasiados intentos. Acceso denegado.")
